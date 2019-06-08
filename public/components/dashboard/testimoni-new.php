659bde0b1ea5e210d93583af1dc09676e604a8bc
Brati Dewi <luhbratidewi@gmail.com>
2019-06-08 17:47:43 +0800

    terserahdah

---
+++ public/components/dashboard/testimoni-new.php
@@ -0,0 +1,56 @@
+<div class="form">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<h1> Welcome Back, Administrator</h1>
+				</div>
+			</div>
+
+
+			<div class="team">
+				<div class="row">
+					<div class="col-12">
+						<ul class="menu">
+							<li> <a href="" class="text-black"> Manage Package</a></li>
+							<li> <a href="" class="text-black"> Manage Team</a></li>
+							<li class="active"> <a href="" class="text-black"> Manage Testimoni</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="row">
+				<div class="col-6">
+					<h2> New Data</h2><br>
+
+
+					<form action="" method="post" enctype="multipart/form-data">
+
+
+						<div class="form-group">
+							<label> Nama </label>
+							<input type="text" name="">
+						</div>
+
+						<div class="form-group">
+							<label><br> Testimoni </label><br><br>
+							<textarea name="komentar" rows="10"></textarea>
+						</div>
+
+						<label><br> Upload Foto </label><br><br>
+
+						<input type="file" name="file_gambar" accept="/*">
+
+
+						<div class="row mt-2">
+							<div class="col-6">
+								<button type="submit" class="btn btn-primary"> Simpan</button>
+								<a href="" class="btn btn-danger ml-2"> Cancel</a>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
---
+++ public/components/gallery.php
@@ -0,0 +1,40 @@
+<section id="galleries">
+
+    <!-- Photo Galleries -->
+    <div class="gallery">
+
+        <!-- Filters -->
+        <header>
+            <h1>Gallery</h1>
+
+        </header>
+
+        <div class="content">
+            <div class="media all people">
+                <a href="assets/images/fulls/a1.jpg"><img src="assets/images/thumbs/a1.jpg" /></a>
+            </div>
+            <div class="media all place">
+                <a href="assets/images/fulls/05.jpg"><img src="assets/images/thumbs/05.jpg" /></a>
+            </div>
+            <div class="media all thing">
+                <a href="assets/images/fulls/a5.jpg"><img src="assets/images/thumbs/a5.jpg" /></a>
+            </div>
+            <div class="media all people">
+                <a href="assets/images/fulls/a8.jpg"><img src="assets/images/thumbs/a8.jpg" /></a>
+            </div>
+            <div class="media all place">
+                <a href="assets/images/fulls/06.jpg"><img src="assets/images/thumbs/06.jpg" /></a>
+            </div>
+            <div class="media all thing">
+                <a href="assets/images/fulls/a6.jpg"><img src="assets/images/thumbs/a6.jpg" /></a>
+            </div>
+            <div class="media all people">
+                <a href="assets/images/fulls/a10.jpg"><img src="assets/images/thumbs/a10.jpg" /></a>
+            </div>
+            <div class="media all place">
+                <a href="assets/images/fulls/07.jpg"><img src="assets/images/thumbs/07.jpg" /></a>
+            </div>
+
+        </div>
+    </div>
+</section>
